99573bff62e74bfb8a71962a1712a739138e72ba
owen-chw <c040466bo@gmail.com>
2025-05-10 16:52:38 +0800

    add GET /api/leave-requests/team

--- app/routes/leave.py
+++ app/routes/leave.py
@@ -5,7 +5,7 @@ from datetime import date
 from ..crud import user as user_crud
 from ..crud import leave as leave_crud
 from ..schemas.user import UserOut, TeamListResponse
-from ..schemas.leave import LeaveRequestDetail, LeaveRequestOut, LeaveRequestCreate, LeaveRequestListResponse
+from ..schemas.leave import LeaveRequestDetail, LeaveRequestOut, LeaveRequestCreate, LeaveRequestListResponse, LeaveRequestTeamListResponse
 from ..utils.dependencies import get_current_user
 from ..models.user import User
 from ..database import get_db
@@ -55,6 +55,49 @@ def list_my_leave_requests(
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+    return {
+        "leave_requests": result["items"],
+        "pagination": {
+            "total": result["total"],
+            "page": result["page"],
+            "per_page": result["per_page"],
+            "total_pages": result["total_pages"]
+        }
+    }
+
+@router.get("/team", response_model=LeaveRequestTeamListResponse)
+def list_team_leave_requests(
+    user_id: Optional[int] = Query(None, description="target user_id"),
+    status: Optional[str] = Query(None, description="Filter"),
+    start_date: Optional[date] = Query(None),
+    end_date: Optional[date] = Query(None),
+    page: int = Query(1, ge = 1),
+    per_page: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+    current_user:  User = Depends(get_current_user)    
+): 
+    """
+    Get list of leave requests for team members.
+    """
+    if not current_user.is_manager:
+        raise HTTPException(status_code=403, detail="Access restricted to managers.")
+    
+    try:
+        result = leave_crud.get_team_leave_requests(
+            db=db,
+            manager_id=current_user.id,
+            user_id=user_id,
+            status=status,
+            start_date=start_date,
+            end_date=end_date,
+            page=page,
+            per_page=per_page
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except PermissionError as e:
+        raise HTTPException(status_code=403, detail=str(e))
+    
     return {
         "leave_requests": result["items"],
         "pagination": {
